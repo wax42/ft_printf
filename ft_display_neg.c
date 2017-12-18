71a813f22fa16f2ad65129b3cc9a77fd17627a69
Victor GUERAND <vguerand@e3r12p5.42.fr>
2017-12-18 15:59:15 +0100

    modif

--- ft_display_neg.c
+++ ft_display_neg.c
@@ -6,24 +6,21 @@
 /*   By: vguerand <vguerand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/12 12:18:53 by vguerand          #+#    #+#             */
-/*   Updated: 2017/12/12 12:34:01 by vguerand         ###   ########.fr       */
+/*   Updated: 2017/12/18 15:58:34 by vguerand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_display_neg(p_f parse, unsigned char *var_char)
+int     ft_display_neg(p_f parse, int size)
 {
     int i;
 
-    i = ft_strlen((const char *)var_char);
-    if (parse.neg_int == 1)
-            ft_putchar('-');
-    ft_putstr((const char *)var_char);
+    i = size - parse.neg.width;
     while (i < parse.neg.width)
     {
             ft_putchar(' ');
             i++;
     }
-    return (i > 0) ? 1 : 0;
+    return (i > 0) ? i : 0;
 }
