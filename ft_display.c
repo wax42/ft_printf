e30c2615c95e6269c502b6439aeab8d0132e7d37
Manu BARTHE <mbarthe@e3r11p6.42.fr>
2017-12-19 02:21:10 +0100

    gerer les longs

--- ft_display.c
+++ ft_display.c
@@ -17,26 +17,29 @@ int ft_display(const char *restrict format, va_list *ap, p_f parse)
     if ((parse.val_ret = ft_display_cs(format, ap, parse)))
         return (parse.val_ret);
 
-/*
-    if ((val_ret = ft_display_diox_hh())) //gere le h et le hh
-        return (val_ret);
-    if ((val_ret = ft_display_diox_ll())) // gere le l et le ll
-        return (val_ret);
-    if ((val_ret = ft_display_diox_j()))
-        return (val_ret);
-    if ((val_ret = ft_display_diox_z()))
-        return (val_ret);
-    if ((val_ret = ft_display_diox_none()))
-        return (val_ret);
-	if ((val_ret = ft_display_u_hh())) //gere le h et le hh
-        return (val_ret);
+    if ((parse.val_ret = ft_display_di_hh(format, ap, parse)))
+        return (parse.val_ret);
+        // dDioOuxX gérés tous ensemble
+    if ((parse.val_ret = ft_display_di_ll(format, ap, parse)))
+        return (parse.val_ret);
+        // dDioOxX pas geres ensemble a cause du long (besoin d'un itoa base long)
+    if ((parse.val_ret = ft_display_di_j(format, ap, parse)))
+        return (parse.val_ret);
+        //pareil (intmax_t)
+
+    if ((parse.val_ret = ft_display_di_z(format, ap, parse)))
+        return (parse.val_ret);
+        //pareil (size_t)
+
+    if ((parse.val_ret = ft_display_di_none(format, ap, parse)))
+        return (parse.val_ret);
+        // dDioOuxX ensemble
+  /*
     if ((val_ret = ft_display_u_ll())) // gere le l et le ll
         return (val_ret);
     if ((val_ret = ft_display_u_j()))
         return (val_ret);
     if ((val_ret = ft_display_u_z()))
-        return (val_ret);
-    if ((val_ret = ft_display_u_none()))
         return (val_ret);
     if ((val_ret = ft_display_p_none()))
         return (val_ret);
