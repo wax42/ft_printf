efc6e52f3983b119d898af03d005d5ce0818873a
Victor GUERAND <vguerand@e1r13p1.42.fr>
2018-01-19 00:32:07 +0100

    a la norme

--- ft_display_di_j.c
+++ ft_display_di_j.c
@@ -6,13 +6,31 @@
 /*   By: vguerand <vguerand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/11 16:36:11 by vguerand          #+#    #+#             */
-/*   Updated: 2018/01/18 21:47:53 by mbarthe          ###   ########.fr       */
+/*   Updated: 2018/01/18 23:35:27 by vguerand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int		ft_display_di_j(va_list *ap, p_f parse)
+static void	ft_display_di_j2(int sa, char *str, p_f *parse)
+{
+	if (parse->type == 'X' && parse->space.width)
+	{
+		str = ft_itoa_base_max(18446744073709551615U - sa, 16, 1);
+		parse->space.width = 0;
+	}
+	else
+		(parse->type == 'X' ? str = ft_itoa_base_max(sa, 16, 1) : str);
+	parse->val_ret = aff_struct((int)ft_strlen(str), parse);
+	parse->val_ret = ft_display_char(str, *parse);
+	if (parse->neg.val && parse->precision.width <= parse->neg.width)
+		parse->val_ret += ft_display_c(find_nbr(*parse, parse->neg.width - \
+			parse->htag.width + parse->plus, (int)ft_strlen(str)), 32);
+	if (str[0] != '\0')
+		ft_strdel(&str);
+}
+
+int			ft_display_di_j(va_list *ap, p_f parse)
 {
 	intmax_t	sa;
 	uintmax_t	la;
@@ -26,24 +44,14 @@ int		ft_display_di_j(va_list *ap, p_f parse)
 	if (!(ft_cchr("j", parse.flag.val)))
 		return (0);
 	sa = (uintmax_t)va_arg(*ap, long long int);
-	(sa < 0 && parse.type != 'u') ? (parse.space.width = 1) : (parse.space.width = 0);
+	(sa < 0 && parse.type != 'u') ? (parse.space.width = 1) : parse.space.width;
 	(parse.space.width) ? la = -sa : sa;
-	(parse.space.width && (parse.type == 'o' || parse.type == 'x' || parse.type == 'X')) ? (parse.space.width = 0) : sa;
-	(parse.type == 'd' || parse.type == 'i' || parse.type == 'u') ? str = ft_itoa_base_max(sa, 10, 0) : str;
+	(parse.space.width && (parse.type == 'o' || parse.type == 'x' || parse.type\
+	== 'X')) ? (parse.space.width = 0) : sa;
+	(parse.type == 'd' || parse.type == 'i' || parse.type == 'u') ? str = \
+	ft_itoa_base_max(sa, 10, 0) : str;
 	(parse.type == 'o') ? str = ft_itoa_base_max(sa, 8, 0) : str;
 	(parse.type == 'x') ? str = ft_itoa_base_max(sa, 16, 0) : str;
-	if (parse.type == 'X' && parse.space.width)
-	{
-		str = ft_itoa_base_max(18446744073709551615U - sa, 16, 1);
-		parse.space.width = 0;
-	}
-	else
-		(parse.type == 'X' ? str = ft_itoa_base_max(sa, 16, 1) : str);
-	parse.val_ret = aff_struct((int)ft_strlen(str), &parse);
-	parse.val_ret = ft_display_char(str, parse);
-	if (parse.neg.val && parse.precision.width <= parse.neg.width)
-		parse.val_ret += ft_display_c(find_nbr(parse, parse.neg.width - parse.htag.width + parse.plus, (int)ft_strlen(str)), 32);
-	if (str[0] != '\0')
-		ft_strdel(&str);
+	ft_display_di_j2(sa, str, &parse);
 	return (parse.val_ret);
 }
--- ft_display_di_l.c
+++ ft_display_di_l.c
@@ -6,13 +6,62 @@
 /*   By: vguerand <vguerand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/11 16:36:11 by vguerand          #+#    #+#             */
-/*   Updated: 2018/01/18 21:52:19 by mbarthe          ###   ########.fr       */
+/*   Updated: 2018/01/19 00:12:18 by vguerand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int		ft_display_di_l(va_list *ap, p_f parse)
+static char	*ft_display_di_l3(p_f *parse, long int sa, unsigned long long *la, \
+	char *str)
+{
+	(sa < 0) ? (parse->space.width = 1) : parse->space.width;
+	if (sa == 0)
+	{
+		if (parse->type != 'x')
+		{
+			(parse->precision.val == 1 && parse->precision.width == 0) ? (str =\
+				"") : str;
+			parse->neutral = 1;
+			if ((parse->type == 'o' || parse->type == 'O') && parse->htag.val)
+				parse->neutral = 0;
+		}
+	}
+	else
+	{
+		if (parse->space.width)
+			*la = -sa;
+		else
+			(*la = sa);
+	}
+	return (str);
+}
+
+static void	ft_display_di_l2(char *str, p_f *parse)
+{
+	parse->val_ret = aff_struct((int)ft_strlen(str), parse);
+	parse->val_ret = ft_display_char(str, *parse);
+	if (parse->neg.val && parse->precision.width <= parse->neg.width)
+		parse->val_ret += ft_display_c(find_nbr(*parse, parse->neg.width - \
+			parse->htag.width + parse->plus, (int)ft_strlen(str)), 32);
+	if (str[0] != '\0')
+		ft_strdel(&str);
+}
+
+static int	ft_condition(p_f *parse)
+{
+	if (ft_cchr("DUO", parse->type))
+	{
+		parse->flag.val = 'l';
+		parse->flag.width = 1;
+	}
+	if (!(ft_cchr("DUdiuoOxX", parse->type)) || (!(ft_cchr("l", \
+	parse->flag.val)) || parse->flag.width < 1))
+		return (0);
+	return (1);
+}
+
+int			ft_display_di_l(va_list *ap, p_f parse)
 {
 	long int				sa;
 	unsigned long long int	la;
@@ -21,43 +70,21 @@ int		ft_display_di_l(va_list *ap, p_f parse)
 	la = 0;
 	sa = 0;
 	str = NULL;
-	if (ft_cchr("DUO", parse.type))
-	{
-		parse.flag.val = 'l';
-		parse.flag.width = 1;
-	}
-	if (!(ft_cchr("DUdiuoOxX", parse.type)))
+	if (ft_condition(&parse) == 0)
 		return (0);
-	if (!(ft_cchr("l", parse.flag.val)))
-		return (0);
-	if (parse.flag.width >= 1)
-	{
-		(parse.type == 'u' || parse.type == 'U' || parse.type == 'x' || parse.type == 'X') ? la = va_arg(*ap, unsigned long int) : sa;
-		(parse.type == 'o' || parse.type == 'O') ? la = va_arg(*ap, unsigned long int) : sa;
-		(parse.type == 'd' || parse.type == 'i' || parse.type == 'D') ? sa = va_arg(*ap, long int) : sa;
-		(sa < 0) ? (parse.space.width = 1) : (parse.space.width = 0);
-		if (sa == 0)
-		{
-			if (parse.type != 'x')
-			{
-				(parse.precision.val == 1 && parse.precision.width == 0) ? (str = ""): str;
-				parse.neutral = 1;
-				if ((parse.type == 'o' || parse.type == 'O') && parse.htag.val)
-					parse.neutral = 0;
-			}
-		}
-		else
-			(parse.space.width) ? (la = -sa) : (la = sa);
-		(parse.type == 'x' ? str = ft_itoa_base_max(la, 16, 0) : str);
-		(parse.type == 'X' ? str = ft_itoa_base_max(la, 16, 1) : str);
-		(parse.type == 'd' || parse.type == 'i' || parse.type == 'u' || parse.type == 'D' || parse.type == 'U') ? str = ft_itoa_base_max(la, 10, 0) : str;
-		((parse.type == 'o' || parse.type == 'O') && str == NULL) ? str = ft_itoa_base_max(la, 8, 0) : str;
-		parse.val_ret = aff_struct((int)ft_strlen(str), &parse);
-		parse.val_ret = ft_display_char(str, parse);
-		if (parse.neg.val && parse.precision.width <= parse.neg.width)
-			parse.val_ret += ft_display_c(find_nbr(parse, parse.neg.width - parse.htag.width + parse.plus, (int)ft_strlen(str)), 32);
-	}
-	if (str[0] != '\0')
-		ft_strdel(&str);
+	(parse.type == 'u' || parse.type == 'U' || parse.type == 'x' || \
+	parse.type == 'X') ? la = va_arg(*ap, unsigned long int) : sa;
+	(parse.type == 'o' || parse.type == 'O') ? la = va_arg(*ap, \
+		unsigned long int) : sa;
+	(parse.type == 'd' || parse.type == 'i' || parse.type == 'D') ?\
+	sa = va_arg(*ap, long int) : sa;
+	str = ft_display_di_l3(&parse, sa, &la, str);
+	(parse.type == 'x' ? str = ft_itoa_base_max(la, 16, 0) : str);
+	(parse.type == 'X' ? str = ft_itoa_base_max(la, 16, 1) : str);
+	(parse.type == 'd' || parse.type == 'i' || parse.type == 'u' || parse.type \
+	== 'D' || parse.type == 'U') ? str = ft_itoa_base_max(la, 10, 0) : str;
+	((parse.type == 'o' || parse.type == 'O') && str == NULL) ? str = \
+	ft_itoa_base_max(la, 8, 0) : str;
+	ft_display_di_l2(str, &parse);
 	return (parse.val_ret);
 }
